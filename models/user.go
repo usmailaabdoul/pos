84a5989751554959020ffd9dd513c89fca2d8e11
Acha Bill <achabill12@gmail.com>
2020-09-22 23:25:53 +0100

    remove password from json response

--- models/user.go
+++ models/user.go
@@ -10,7 +10,7 @@ import (
 type User struct {
 	ID          primitive.ObjectID   `bson:"_id" json:"_id"`
 	Username    string               `bson:"username" json:"username"`
-	Password    string               `bson:"password" json:"password"`
+	Password    string               `bson:"password" json:"-"`
 	Name        string               `bson:"name" json:"name"`
 	Roles       []primitive.ObjectID `bson:"roles" json:"roles"`
 	PhoneNumber string               `bson:"phoneNumber" json:"phoneNumber"`
