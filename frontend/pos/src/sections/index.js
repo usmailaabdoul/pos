e5f3ecb7eed4bd2543104954b089d43911418daa
usmailaabdoul <ismaelabdul77@gmail.com>
2020-10-03 01:50:30 +0100

    Adding customer report page

--- frontend/pos/src/sections/index.js
+++ frontend/pos/src/sections/index.js
@@ -1,5 +1,6 @@
 import Dashboard from './dashboard/dashboard';
 import Sales from './sales/sales';
 import SaleDetailReport from './saledetails/saledetails'
+import Customer from './customer/customer';
 
-export { Dashboard, Sales, SaleDetailReport }
+export { Dashboard, Sales, SaleDetailReport, Customer }
